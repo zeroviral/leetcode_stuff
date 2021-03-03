4a38be6b937e2d385ca64fd824b2c8d0d9a19ebb
zeroviral <kshari7@msn.com>
2021-03-03 14:52:59 -0500

    updating one line swag

--- most-frequent-subtree-sum/most-frequent-subtree-sum.py
+++ most-frequent-subtree-sum/most-frequent-subtree-sum.py
@@ -6,28 +6,26 @@
 #         self.right = right
 from collections import defaultdict
 
+
 class Solution:
     def findFrequentTreeSum(self, root: TreeNode) -> List[int]:
-        
+
         if not root:
             return []
-        
-        self.lookup = defaultdict()
+
+        self.lookup = defaultdict(int)
+
         def dfs(root):
-            
+
             if not root:
                 return 0
-            
+
             total = dfs(root.left) + dfs(root.right) + root.val
-            
-            if total in self.lookup:
-                self.lookup[total] += 1
-            else:
-                self.lookup[total] = 1
-            
+            self.lookup[total] += 1
+
             return total
-        
+
         dfs(root)
         maxVal = max(self.lookup.values())
-        
+
         return [key for key, value in self.lookup.items() if value == maxVal]
