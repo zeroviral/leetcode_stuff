1cd1585ed54d0041ce257c2f9bd7b0ca6812928d
zeroviral <kshari7@msn.com>
2021-03-06 22:07:24 -0500

    updating one line swag

--- path-with-maximum-gold/path-with-maximum-gold.py
+++ path-with-maximum-gold/path-with-maximum-gold.py
@@ -1,32 +1,27 @@
 class Solution:
     def getMaximumGold(self, grid: List[List[int]]) -> int:
-        
-        self.maxVal = 0
-        ans = set()
-        
-        def dfs(grid, i, j, n, m, total):
-            
+
+        def dfs(grid, i, j, total):
+
             if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0:
                 return 0
-            
+
             total += grid[i][j]
             temp = grid[i][j]
             grid[i][j] = 0
-            currMax = max(total, dfs(grid, i+1, j, m, n, total), dfs(grid, i-1, j, m, n, total), dfs(grid, i, j+1, m, n, total), dfs(grid, i, j-1, m, n, total))
+            currMax = max(total, dfs(grid, i + 1, j, total),
+                          dfs(grid, i - 1, j, total),
+                          dfs(grid, i, j + 1, total),
+                          dfs(grid, i, j - 1, total))
             grid[i][j] = temp
-            
-            ans.add(currMax)
             return currMax
-        
-        
-        
-        n, m = len(grid), len(grid[0])
-        
-        
-        for row in range(n):
-            for col in range(m):
-                
+
+        maxVal = 0
+
+        for row in range(len(grid)):
+            for col in range((len(grid[0]))):
+
                 if grid[row][col] > 0:
-                    self.maxVal = dfs(grid, row, col, n, m, 0)
-        
-        return max(ans)
+                    maxVal = max(maxVal, dfs(grid, row, col, 0))
+
+        return maxVal
