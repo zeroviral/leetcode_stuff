2a811d1bb40950ae5a75f2043ef1e74d94b489c5
zeroviral <kshari7@msn.com>
2021-03-11 13:44:46 -0500

    Time: 3868 ms (5.02%), Space: 15.6 MB (79.92%) - LeetHub

--- word-search/word-search.py
+++ word-search/word-search.py
@@ -1,27 +1,27 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-
-        def dfs(grid, i, j, count, target):
+        
+        rows, cols = len(board), len(board[0])
+        
+        def dfs(grid, i, j, count, word):
             
-            if count == len(target):
+            if count == len(word):
                 return True
             
-            if i < 0 or i >= len(grid) or j < 0 or j >=len(grid[i]) or grid[i][j] != target[count]:
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[i]) or grid[i][j] != word[count]:
                 return False
             
             temp = grid[i][j]
-            grid[i][j] = ''
-            found = dfs(grid, i+1, j, count + 1, target) or dfs(grid, i-1, j, count + 1, target) or dfs(grid, i, j+1, count + 1, target) or dfs(grid, i, j-1, count + 1, target)
-            board[i][j] = temp
-            
+            grid[i][j] = "0"
+            count += 1
+            found = dfs(grid, i+1, j, count, word) or dfs(grid, i-1, j, count, word) or dfs(grid, i, j+1, count, word) or dfs(grid, i, j-1, count, word)
+            grid[i][j] = temp
             return found
         
-        
-        
-        for i in range(len(board)):
-            for j in range(len(board[i])):
-                
+        for i in range(rows):
+            for j in range(cols):
                 if board[i][j] == word[0] and dfs(board, i, j, 0, word):
                     return True
         
         return False
+                    
