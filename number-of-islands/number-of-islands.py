eff6f735b72cc878335e7bf1cfd1ff68fdc0142b
zeroviral <kshari7@msn.com>
2021-03-19 15:53:21 -0400

    Time: 132 ms (86.80%), Space: 15.3 MB (72.32%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -4,7 +4,6 @@ class Solution:
         rows, cols = len(grid), len(grid[0])
         count = 0
         
-        
         def dfs(grid, i, j):
             if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == '0':
                 return
@@ -14,11 +13,8 @@ class Solution:
             dfs(grid, i, j+1)
             dfs(grid, i, j-1)
         
-        
-        
         for row in range(rows):
-            for col in range(cols):
-                
+            for col in range(cols):        
                 if grid[row][col] == '1':
                     count += 1
                     dfs(grid, row, col)
