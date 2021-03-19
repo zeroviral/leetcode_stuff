322a86584cb4d9c08a179eb87f35855f9222dc7d
zeroviral <kshari7@msn.com>
2021-03-19 19:28:12 -0400

    Time: 96 ms (39.77%), Space: 14.3 MB (64.99%) - LeetHub

--- permutation-in-string/permutation-in-string.py
+++ permutation-in-string/permutation-in-string.py
@@ -5,13 +5,11 @@ class Solution:
         count = 0
         
         for r in range(len(s2)):
-            
             if s2[r] in lookup:
                 if lookup[s2[r]] > 0:
                     count += 1
                 lookup[s2[r]] -= 1
-                
-            while r - l + 1 > len(s1):
+            while l <= r and r - l + 1 > len(s1):
                 if s2[l] in lookup:
                     lookup[s2[l]] += 1
                     if lookup[s2[l]] > 0:
