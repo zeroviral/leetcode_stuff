690420ee2432f00ce45054c7ebcef58e8630ab89
zeroviral <kshari7@msn.com>
2021-03-22 16:40:23 -0400

    Time: 208 ms (50.33%), Space: 16.2 MB (12.50%) - LeetHub

--- set-mismatch/set-mismatch.py
+++ set-mismatch/set-mismatch.py
@@ -1,6 +1,9 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
+        cnts = collections.Counter(nums)
         
-        l, s = len(nums), sum(set(nums))
-        l = l * ( 1 + l ) // 2
-        return [sum(nums) - s, l - s]
+        dup = cnts.most_common()[0][0]
+        
+        for i in range(1,len(nums)+1):
+            if i not in cnts:
+                return [dup, i]
