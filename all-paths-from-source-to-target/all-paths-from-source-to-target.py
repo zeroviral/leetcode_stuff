ff6052886fdc1501f1eb2de05d24cfdc8e0d687c
zeroviral <kshari7@msn.com>
2021-03-28 16:28:03 -0400

    Time: 116 ms (15.45%), Space: 15.7 MB (43.05%) - LeetHub

--- all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -2,15 +2,17 @@ from collections import deque
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         ans = []
-        path = deque([0])
+        path = deque()
+        path.append(0)
         target = len(graph) - 1
         
         def dfs(node, path):
             
+            
             if node == target:
                 ans.append(list(path))
                 return
-                
+            print(f'its currently lookin like: dqeue: {path}')
             for nextNode in graph[node]:
                 path.append(nextNode)
                 dfs(nextNode, path)
