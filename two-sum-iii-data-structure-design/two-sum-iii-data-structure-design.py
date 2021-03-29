554518ed9ce311e80cfcfcf95eb8e6ea17b499a4
zeroviral <kshari7@msn.com>
2021-03-28 20:48:22 -0400

    Time: 356 ms (64.05%), Space: 20.7 MB (16.04%) - LeetHub

---
+++ two-sum-iii-data-structure-design/two-sum-iii-data-structure-design.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+class TwoSum:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.lookup = defaultdict(int)
+
+    def add(self, number: int) -> None:
+        """
+        Add the number to an internal data structure..
+        """
+        self.lookup[number] += 1
+        
+
+    def find(self, value: int) -> bool:
+        """
+        Find if there exists any pair of numbers which sum is equal to the value.
+        """
+        for element in self.lookup.keys():
+            diff = value - element
+            if element != diff:
+                if diff in self.lookup:
+                    return True
+            elif self.lookup[element] > 1:
+                return True
+        return False
+            
+
+
+# Your TwoSum object will be instantiated and called as such:
+# obj = TwoSum()
+# obj.add(number)
+# param_2 = obj.find(value)
