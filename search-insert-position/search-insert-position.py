e42049ac431d20315b16f013f4d7193f5d1c7625
zeroviral <kshari7@msn.com>
2021-03-28 23:36:53 -0400

    Time: 44 ms (90.69%), Space: 15 MB (80.22%) - LeetHub

---
+++ search-insert-position/search-insert-position.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            
+            mid = (l + r) // 2
+            
+            if nums[mid] == target:
+                return mid
+            if target < nums[mid]:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l
