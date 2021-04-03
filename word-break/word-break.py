3335cb6a73cac798dc4cfe0c03ceaaee84d806c1
zeroviral <kshari7@msn.com>
2021-04-02 20:18:47 -0400

    Time: 48 ms (17.25%), Space: 14.4 MB (71.18%) - LeetHub

---
+++ word-break/word-break.py
@@ -0,0 +1,6 @@
+class Solution:
+    def wordBreak(self, s, words):
+        ok = [True]
+        for i in range(1, len(s)+1):
+            ok += any(ok[j] and s[j:i] in words for j in range(i)),
+        return ok[-1]
