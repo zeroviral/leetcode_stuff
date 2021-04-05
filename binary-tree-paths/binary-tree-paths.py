398e5ae0acb446370830e518e39d0b98462a9787
zeroviral <kshari7@msn.com>
2021-04-05 12:47:44 -0400

    Time: 28 ms (89.82%), Space: 14.4 MB (29.86%) - LeetHub

--- binary-tree-paths/binary-tree-paths.py
+++ binary-tree-paths/binary-tree-paths.py
@@ -9,24 +9,17 @@ class Solution:
         
         ans = []
         
-        def dfs(root, path):
+        def dfs(root, path=""):
             
             if not root:
                 return
             
-            if not root.right and not root.left:
+            if not root.left and not root.right:
                 ans.append(path + str(root.val))
                 return
-            
-            path += str(root.val) + "->" 
-            
+            path += str(root.val) + "->"
             dfs(root.left, path)
             dfs(root.right, path)
         
-        
-        dfs(root, "")
+        dfs(root)
         return ans
-            
-            
-                
-    
