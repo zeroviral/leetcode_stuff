2e12b08e0d0d31c85fb5fc15ae6851fb457c3ba3
zeroviral <kshari7@msn.com>
2021-04-06 12:45:23 -0400

    Time: 180 ms (91.22%), Space: 15 MB (31.85%) - LeetHub

--- number-of-provinces/number-of-provinces.py
+++ number-of-provinces/number-of-provinces.py
@@ -1,15 +1,18 @@
 class Solution:
-    def findCircleNum(self, A):
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
         seen = set()
+        
         def dfs(node):
-            for curr, nxt in enumerate(A[node]):
+            for curr, nxt in enumerate(isConnected[node]):
                 if nxt and curr not in seen:
                     seen.add(curr)
                     dfs(curr)
-
-        ans = 0
-        for i in range(len(A)):
+        
+        
+        components = 0
+        for i in range(len(isConnected)):
             if i not in seen:
+                components += 1
                 dfs(i)
-                ans += 1
-        return ans
+        return components
+                
