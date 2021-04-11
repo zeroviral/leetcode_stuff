1e5d75a57203463cea18bbcd634fa7836bd48d78
zeroviral <kshari7@msn.com>
2021-04-11 17:45:35 -0400

    Time: 44 ms (77.78%), Space: 14.3 MB (77.61%) - LeetHub

--- sentence-similarity/sentence-similarity.py
+++ sentence-similarity/sentence-similarity.py
@@ -3,12 +3,10 @@ class Solution:
         if len(sentence1) != len(sentence2):
             return False
         
-        sets = set()
-        for word1, word2 in similarPairs:
-            sets.add((word1, word2))
+        seen = set((word1, word2) for word1, word2 in similarPairs)
         
-        for w, v in zip(sentence1, sentence2):
-            if w != v and (w,v) not in sets and (v,w) not in sets:
+        for a, b in zip(sentence1, sentence2):
+            if a != b and (a, b) not in seen and (b, a) not in seen:
                 return False
         return True
         
