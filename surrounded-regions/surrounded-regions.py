08ccce75d2d77cda75e3c3f0407aa186f45082b4
zeroviral <kshari7@msn.com>
2021-04-11 18:58:41 -0400

    Time: 148 ms (29.83%), Space: 16.3 MB (42.37%) - LeetHub

--- surrounded-regions/surrounded-regions.py
+++ surrounded-regions/surrounded-regions.py
@@ -7,19 +7,17 @@ class Solution:
             if not 0 <= row < len(grid) or not 0 <= col < len(grid[0]) or grid[row][col] != 'O':
                 return 
             grid[row][col] = 'D'
-            dfs(row+1, col)
-            dfs(row-1, col)
-            dfs(row, col+1)
-            dfs(row, col-1)
+            for i, j in [(row+1, col), (row-1, col), (row, col+1), (row, col-1)]:
+                dfs(i, j)
         
-        for row, group in enumerate(grid):
-            for col, val in enumerate(group):
-                if row in {0, len(grid)-1} or col in {0, len(grid[0])-1} and val == 'O':
+        rows, cols = len(grid), len(grid[0])
+        for row in range(rows):
+            for col in range(cols):
+                if row == 0 or row == len(grid) - 1 or col == 0 or col == len(grid[0]) - 1 and grid[row][col] == 'O':
                     dfs(row, col)
-        
-        for row, group in enumerate(grid):
-            for col, val in enumerate(group):
-                if val == 'D':
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == 'D':
                     grid[row][col] = 'O'
-                elif val == 'O':
+                elif grid[row][col] == 'O':
                     grid[row][col] = 'X'
