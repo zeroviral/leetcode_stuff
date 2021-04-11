635aca85f40c8ff044ce5366e4ef563789b3001c
zeroviral <kshari7@msn.com>
2021-04-11 19:49:01 -0400

    Time: 588 ms (31.93%), Space: 15.9 MB (61.06%) - LeetHub

--- number-of-enclaves/number-of-enclaves.py
+++ number-of-enclaves/number-of-enclaves.py
@@ -7,14 +7,15 @@ class Solution:
             if not 0 <= i < len(A) or not 0 <= j < len(A[0]) or A[i][j] != 1:
                 return
             A[i][j] = 0
-            for i, j in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
-                dfs(i, j)
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
         
         for row in range(rows):
             for col in range(cols):
                 if row == 0 or row == len(A) - 1 or col == 0 or col == len(A[0]) - 1 and A[row][col] == 1:
                     dfs(row, col)
-        
         
         for row in range(rows):
             for col in range(cols):
