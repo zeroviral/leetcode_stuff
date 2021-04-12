004bfeff96fda6a0911947145106603312c5fda9
zeroviral <kshari7@msn.com>
2021-04-12 17:05:26 -0400

    Time: 180 ms (91.03%), Space: 15.3 MB (13.25%) - LeetHub

--- number-of-provinces/number-of-provinces.py
+++ number-of-provinces/number-of-provinces.py
@@ -1,34 +1,21 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        parents = [i for i in range(len(isConnected) + 1)]
-        priority = [0] * (len(isConnected) + 1)
-        components = len(isConnected)
+        graph = collections.defaultdict(list)
+        provinces = 0
+        visited = set()
         
-        def find(node):
-            if node == parents[node]:
-                return node
-            else:
-                parents[node] = find(parents[node])
-                return parents[node]
+        def dfs(node):
+            for curr, nxt in enumerate(isConnected[node]):
+                if nxt and curr not in visited:
+                    visited.add(curr)
+                    dfs(curr)
         
-        def union(start, end):
-            
-            sParent = find(start)
-            eParent = find(end)
-            if sParent == eParent:
-                return False
-                
-            if priority[sParent] >= priority[eParent]:
-                parents[eParent] = parents[sParent]
-                priority[sParent] += 1
-            else:
-                parents[sParent] = parents[eParent]
-                priority[eParent] += 1
-            return True
         
-        for node, neighbors in enumerate(isConnected):
-            for i in range(len(neighbors)):
-                if node != i and neighbors[i] == 1:
-                    if union(node, i):
-                        components -= 1
+        components = 0
+        for i in range(len(isConnected)):
+            if i not in visited:
+                components += 1
+                dfs(i)
         return components
+        
+        
