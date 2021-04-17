38a021f3221a65828932cf27fdab893296c35ca1
zeroviral <kshari7@msn.com>
2021-04-17 18:46:54 -0400

    Time: 120 ms (61.53%), Space: 16.1 MB (41.37%) - LeetHub

--- minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,15 +1,28 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        indexes_to_remove = set()
+        '''Needs - Removal set, stack.
+        1. Iterate through list, find parenthesis
+        2. Check parenthesis type
+            2.1 If parenthesis is ( then append to stack
+            2.2 if Parenthesis is ) then
+                2.2.1 if stack is empty add index to removals set
+                2.2.2 if stack not empty then append pop from the stack
+        3. Combine the stack and removals set
+        4. Return string composed of all characters not in the union set
+        '''
+        removals = set()
         stack = []
+        
         for i, c in enumerate(s):
             if c not in "()":
                 continue
             if c == "(":
                 stack.append(i)
-            elif not stack:
-                indexes_to_remove.add(i)
+            elif c == ")" and not stack:
+                removals.add(i)
             else:
                 stack.pop()
-        indexes_to_remove = indexes_to_remove.union(set(stack))
-        return "".join([c for i, c in enumerate(s) if i not in indexes_to_remove])
+        removals = removals.union(set(stack))
+        return "".join([c for i, c in enumerate(s) if i not in removals])
+        
+        
