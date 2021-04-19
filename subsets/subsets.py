6d80db86a17de3383340f851e3b0bcdef71713c9
zeroviral <kshari7@msn.com>
2021-04-19 12:21:06 -0400

    Time: 36 ms (48.07%), Space: 14.2 MB (91.95%) - LeetHub

--- subsets/subsets.py
+++ subsets/subsets.py
@@ -1,21 +1,4 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        possibilities = ["{0:b}".format(i).zfill(len(nums)) for i in range(2 ** len(nums))]
-        ans = []
-#         for i in range(2 ** len(nums)):
-# #             Generate string using i
-#             val = "{0:b}".format(i)
-#             possibilities.append(val.zfill(len(nums)))
-        # for i, word in enumerate(possibilities):
-        #     temp = []
-        #     for j in word:
-        #         if int(j):
-        #             temp.append(nums[int(j)])
-        #     ans.append(temp)
-        for combo in possibilities:
-            temp = []
-            for index, c in enumerate(combo):
-                if c == '1':
-                    temp.append(nums[index])
-            ans.append(temp)
-        return ans
+        return [[nums[index] for index, c in enumerate(combo) if c == '1'] for combo in ["{0:b}".format(i).zfill(len(nums)) for i in range(2 ** len(nums))]]
+        
