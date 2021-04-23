e217211778fc5350322c6b016017e5b671dcc1b9
zeroviral <kshari7@msn.com>
2021-04-23 10:07:58 -0400

    Time: 664 ms (25.85%), Space: 14.9 MB (36.69%) - LeetHub

--- max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -3,7 +3,7 @@ class Solution:
         '''
         1. We need: l pointer, count, maxLength
         2. Iterate through the array using a left/right pointer method, and count zeroes.
-            2.1. Once our count reaches the allowed zeroes, (K from input) then we move left pointer up.
+            2.1. Once our count is above the allowed zeroes, (K from input) then we move left pointer up.
                 2.1.1. When we hit our inner while loop to move left up, we check the left pointer index and see if it is 0.
                     2.1.1.1. If we have a zero at the left pointer, we remove 1 from count before incrementing left pointer.
             2.2. Take the max(maxLength, r - l + 1)
