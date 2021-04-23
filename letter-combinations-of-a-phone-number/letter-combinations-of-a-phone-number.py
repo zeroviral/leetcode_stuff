ea524d57965308fa72f8d0766e4b0e11b79904ec
zeroviral <kshari7@msn.com>
2021-04-23 09:24:00 -0400

    Time: 28 ms (81.21%), Space: 14.2 MB (63.03%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -2,15 +2,15 @@ from collections import deque
 
 class Solution(object):
     def letterCombinations(self, digits):
-        if digits is '' : return []
-        kvmaps = {
-            '2': 'abc',
-            '3': 'def',
-            '4': 'ghi',
-            '5': 'jkl',
-            '6': 'mno',
-            '7': 'pqrs',
-            '8': 'tuv',
-            '9': 'wxyz'
-        }
-        return reduce(lambda acc, digit: [x + y for x in acc for y in kvmaps[digit]], digits, [''])
+        if digits == "": return []
+        self.dict = {"1":"", "2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs","8":"tuv","9":"wxyz","10":" "}
+        result = [""]
+        for digit in digits:
+            lst = self.dict[digit]
+            newresult = []
+            for char in lst:
+                for str in result:
+                    newresult.append(str+char)
+            result = newresult
+        return result
+            
