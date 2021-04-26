7532411195f1e8e5da024f6ed67f6b953b6c3f96
zeroviral <kshari7@msn.com>
2021-04-26 17:26:32 -0400

    Time: 36 ms (48.74%), Space: 14.5 MB (19.32%) - LeetHub

--- subsets/subsets.py
+++ subsets/subsets.py
@@ -1,17 +1,3 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        
-        
-        def dfs(index, combo):
-            if index == len(nums):
-                ans.append(combo.copy())
-                return
-        
-            dfs(index + 1, combo)
-            combo.append(nums[index])
-            dfs(index + 1, combo)
-            combo.pop()
-        
-        ans = []
-        dfs(0, [])
-        return ans
+        return [[nums[index] for index, c in enumerate(combo) if c == '1'] for combo in ["{0:b}".format(i).zfill(len(nums)) for i in range(2 ** len(nums))]]
