e445e2745e998251b8f48b26b5d3fb483ed9caa4
zeroviral <kshari7@msn.com>
2021-04-27 23:08:22 -0400

    Time: 216 ms (15.25%), Space: 23.2 MB (24.71%) - LeetHub

--- letter-tile-possibilities/letter-tile-possibilities.py
+++ letter-tile-possibilities/letter-tile-possibilities.py
@@ -1,19 +1,30 @@
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
         seqs = set()
-        visits = [False] * len(tiles)
+        visits = [0] * len(tiles)
 
         def dfs(seq: str, depth: int):
             if seq:
+                print(f'Adding: {seq}')
                 seqs.add(seq)
+            
             if depth == len(tiles):
                 return
 
             for i in range(len(tiles)):
                 if not visits[i]:
-                    visits[i] = True
+                    visits[i] += 1
                     dfs(seq + tiles[i], depth + 1)
-                    visits[i] = False
+                    visits[i] -= 1
 
         dfs('', 0)
         return len(seqs)
+    
+    '''
+    
+    Input: AAB
+    visited = [0, 1, 0]
+    
+    combos = (A, AA, AAB, AB)
+    seq = "A"
+    '''
