4307466e9be62aff658ac808339d5131c993a215
zeroviral <kshari7@msn.com>
2021-04-30 15:47:19 -0400

    Time: 116 ms (69.29%), Space: 16.1 MB (53.99%) - LeetHub

--- minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
+++ minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.py
@@ -1,24 +1,26 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        '''Needs - Removal set, stack.
-        1. Iterate through list, find parenthesis
-        2. Check parenthesis type
-            2.1 If parenthesis is ( then append to stack
-            2.2 if Parenthesis is ) then
-                2.2.1 if stack is empty add index to removals set
-                2.2.2 if stack not empty then append pop from the stack
-        3. Combine the stack and removals set
-        4. Return string composed of all characters not in the union set
+        '''
+        Needs - Removal set, stack.
+        The main idea is we want to record the index positions that are going to be in the returnString.
+        1. Iterate through the string and check to see if each element is in '()'
+            1.1. If the character isn't in the '()', we need to continue (skip to next position)
+            1.2. If the character is a '(' we need to append it to our stack.
+            1.3. If the character is a ')':
+                1.3.1. If the stack is not empty, we will pop cause it matches a parenthesis.
+                1.3.2. If the stack is empty we need to add the position to our removals set - it won't be in the returnString
+            1.4. Create a union set of the stack, and the removals set. This will show which positions we can't use.
+        2. Iterate through the input string, and add each c if c not in removals set.
+        3. Return the recomposed string.
         '''
         removals = set()
         stack = []
-        
         for i, c in enumerate(s):
             if c not in "()":
                 continue
-            if c == "(":
+            if c == '(':
                 stack.append(i)
-            elif not stack and c == ")":
+            elif not stack:
                 removals.add(i)
             else:
                 stack.pop()
