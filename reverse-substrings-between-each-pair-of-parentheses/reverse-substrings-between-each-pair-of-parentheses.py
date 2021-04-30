e33aa437dfc32f4376144ab6bfd6ed338ed29db1
zeroviral <kshari7@msn.com>
2021-04-30 17:51:41 -0400

    Time: 20 ms (98.36%), Space: 14.2 MB (79.02%) - LeetHub

---
+++ reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverseParentheses(self, s):
+        opened = []
+        pair = {}
+        for i, c in enumerate(s):
+            if c == '(':
+                opened.append(i)
+            if c == ')':
+                j = opened.pop()
+                pair[i], pair[j] = j, i
+        res = []
+        i, d = 0, 1
+        while i < len(s):
+            if s[i] in '()':
+                i = pair[i]
+                d = -d
+            else:
+                res.append(s[i])
+            i += d
+        return ''.join(res)
