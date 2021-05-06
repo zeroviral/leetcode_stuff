055c7ff92c7357e0d59eb8f0c5f5512e1f4d90f1
zeroviral <kshari7@msn.com>
2021-05-06 16:41:40 -0400

    Time: 240 ms (55.20%), Space: 20.9 MB (93.40%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.py
+++ product-of-array-except-self/product-of-array-except-self.py
@@ -5,12 +5,12 @@ class Solution:
 
         prod = 1
         for i in range(1,n):
-            prod = prod * nums[i-1]
+            prod *= nums[i-1]
             output[i] *= prod
 
         prod = 1
         for i in range(n-2,-1,-1):
-            prod = prod * nums[i+1]
+            prod *= nums[i+1]
             output[i] *= prod
 
         return output
