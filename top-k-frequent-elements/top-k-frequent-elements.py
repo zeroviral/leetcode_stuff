1f6895e3c599a3ccfdb5476ee1a7c4cf26e2e9f6
zeroviral <kshari7@msn.com>
2021-05-23 10:45:51 -0400

    Time: 100 ms (68.27%), Space: 18.8 MB (49.89%) - LeetHub

--- top-k-frequent-elements/top-k-frequent-elements.py
+++ top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,9 +1,12 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        '''
-        1. Put everything from nums into a dictionary, with mapping as: <number : count>
-        2. Put the counts into a heap.
-            2.1. While the len(heap) > k, we will heappop() our heap, which removes smallest element.
-        3. Return the heap.
-        '''
-        return heapq.nlargest(k, Counter(nums).keys(), Counter(nums).get)
+        heap = []
+        counts = Counter(nums)
+        
+        for key in counts.keys():
+            if len(heap) == k:
+                heapq.heappushpop(heap, (counts[key], key))
+            else:
+                heapq.heappush(heap, (counts[key], key))
+        
+        return [num for (_, num) in heap]
